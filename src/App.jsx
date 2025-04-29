3ef2b3370691df21e97070c9374e47bb45547c2c
MartinAle90 <martinale90@gmail.com>
2025-04-29 16:02:26 -0300

    [feature] Se agrega la visualizacion por pantalla. Mejora de visualizacion por consola. Funcionalidad para eliminar producto [Daza Martin]

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './assets/components/Producto.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>Producto.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
