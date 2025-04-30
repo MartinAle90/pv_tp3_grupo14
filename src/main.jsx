e3ba675c43f77cca45b6c9986ac367be8c5c9c78
MartinAle90 <martinale90@gmail.com>
2025-04-30 12:54:31 -0300

    [fix] Se reparan conflictos del punto Tarea [Daza Martin]

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/css/producto.css'
 import Producto from './assets/components/Producto.jsx'
 import ListaTareas from './assets/components/ListaTareas.jsx'
-import './assets/CSS/index.css'
+import Tasks from './assets/components/Tasks.jsx'
+import Tarea from './assets/components/Tarea.jsx'
+import './assets/css/Tasks.css'
+import './assets/css/Producto.css'
 import App from './App.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Producto />    
-        <ListaTareas />
+        <Tarea />
     </StrictMode>
+    
 
 )
