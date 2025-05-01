b13958d96ac6a6f894ce869b830dc71e2b9ed8b8
MartinAle90 <martinale90@gmail.com>
2025-05-01 19:19:53 -0300

    Nueva forma de mostrar la lista de productos. Se agregan botones para las diferentes funcionalidades [Daza Martin]

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Producto from './assets/components/Producto.jsx'
-import ListaTareas from './assets/components/ListaTareas.jsx'
-import Tasks from './assets/components/Tasks.jsx'
 import Tarea from './assets/components/Tarea.jsx'
 import './assets/css/Tasks.css'
 import './assets/css/Producto.css'
-import App from './App.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Producto />    
-        <Tarea />
+        <Producto/>
+        <Tarea/>
     </StrictMode>
-    
-
 )
